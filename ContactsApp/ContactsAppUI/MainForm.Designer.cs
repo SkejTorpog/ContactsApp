37feb59cab4e6b37bfb014f7b446c39dba9bb93f
Маматкулов <farmamatkulov@mail.ru>
2020-11-11 14:21:33 +0700

    Добавлен проект логики

--- ContactsApp/ContactsAppUI/MainForm.Designer.cs
+++ ContactsApp/ContactsAppUI/MainForm.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace ContactsAppUI
+﻿using ContactsApp;
+namespace ContactsAppUI
 {
     partial class MainForm
     {
@@ -28,10 +29,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
